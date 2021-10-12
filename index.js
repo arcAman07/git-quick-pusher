8d81a1439cbc61a7aa9f4590461dad9a3228f2fc
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-12 18:28:10 +0530

    index.js Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 #! /usr/bin/env node
 const shell = require("shelljs");
 const Color = require("color");
+var currentBranch = "master";
+var commitMessage;
+// Commands
+// 1)git add .
+// 2)git commit -m ""
+// 3)git push origin master(branch)
+
+// More Features/Versions:
+// 1) automatically reads the branch from git and updates it.
+// 2) More cli tools like help, version ,etc
+// 3) Getting deafult commit messages from good sources.
+//4) Other git commands helpline like merge, pull, fetch , etc
+// 5) Inbuilt feature to save the code when file changed.(maybe another package)
+// 6) Can use it also directly from vs code rather than CLI by making a file saving that command and writing commmit messages.
