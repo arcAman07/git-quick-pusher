65f8972add79989a8895a61c8edbd7ae6df7f100
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-13 15:52:25 +0530

    hello world

--- index.js
+++ index.js
@@ -23,15 +23,6 @@ console.log(secondCommand);
 // 2)git commit -m ""
 // 3)git push origin master(branch)
 
-// More Features/Versions:
-// 1) automatically reads the branch from git and updates it.
-// 2) More cli tools like help, version ,etc
-// 3) Getting deafult commit messages from good sources.
-//4) Other git commands helpline like merge, pull, fetch , etc
-// 5) Inbuilt feature to save the code when file changed.(maybe another package)
-// 6) Can use it also directly from vs code rather than CLI by making a file saving that command and writing commmit messages.
-// 7) Create custom cli through github clone and also make it in it rush(cargo) and golang
-
 // To push an existing repository
 const push = async () => {
   await shell.exec(firstCommand);
@@ -39,10 +30,12 @@ const push = async () => {
   await shell.exec(thirdCommand);
 };
 
-// program
-//   .command("push <commitMessage>")
-//   .description("Push a commit message")
-//   .action(push);
+// First time - connect git to github repository
+//Format - push "commitMessage" url
+
+// To set the current branch of the working repo
+// Format - set branchName(currentBranch)
+// default working branch is "master"
 
 // gitquickpusher-1.0.0.tgz
 
